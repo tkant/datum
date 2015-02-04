217320b47f197eba314d6361065de7676c24d62d
David White <david.white@spry-soft.com>
2015-02-04 21:55:49 +0000

    Added CORS support

--- app/app.js
+++ app/app.js
@@ -4,6 +4,13 @@ var app        = require('express')(),
 
 // Setup required middleware
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+});
 
 // Setup resource routes
 app.use('/', routes);
