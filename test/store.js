7ef63719921c328e11fe7367e70d888e9a683bae
David White <david.white@spry-soft.com>
2015-02-05 20:57:59 +0000

    Added basic mocha test file with chaijs example

---
+++ test/store.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect,
+    store  = require('../app/store');
+
+
+describe('store', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      expect([1,2,3].indexOf(-1)).to.equal(-1);
+      expect([1,2,3].indexOf(-1)).to.equal(-1);
+    })
+  })
+}) 
